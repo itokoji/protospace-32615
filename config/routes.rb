71acd6ae59274e31808e9e07fb56a0a1fd68b7bb
itokoji <gyiteng473@gmail.com>
2020-12-20 14:57:49 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "prototypes#index"
+  resources :users, only: :show
+  resources :prototypes
+  resources :comments, only: :create
+end
