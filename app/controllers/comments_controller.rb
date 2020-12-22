600bbb3ee9573697c7d9569d512df6e2b0f73f06
itokoji <gyiteng473@gmail.com>
2020-12-22 16:19:52 +0900

    修正３

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to prototype_path(@comment_params)
+      redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
@@ -11,6 +11,7 @@ class CommentsController < ApplicationController
   end
 
   private
+  
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
